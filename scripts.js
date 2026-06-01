ff79afb23d1fd876126adec92d69d53b67d161d5
FelipeGabriel18 <felipegsq2@gmail.com>
2026-06-01 16:34:28 -0300

    TESTES (Java-Script)

--- scripts.js
+++ scripts.js
@@ -1,13 +1,41 @@
 
-const botaoOn = document.querySelector("#big-button");
+const botaoOn = document.querySelector('#big-button');
 const tela = document.querySelector('#screen');
+
+const botaoOff = document.querySelector('#off');
+const setaCima = document.querySelector('.arrow-up');
+const setaDireita = document.querySelector('.arrow-right');
 
 let IsLigado = false;
 
 botaoOn.addEventListener("click", ()=> { 
     if (!IsLigado){ 
-        botaoOn.classList.add('#aceso');
+        botaoOn.classList.add('aceso');
+        tela.classList.add('aceso');
         tela.innerHTML = '<img src="./assets/pikachu-logo.gif" alt="poke-logo" class="logo-animada">'
-        IsLigado = true;
-    } 
+       IsLigado = true;
+    } else {
+        botaoOn.classList.remove('aceso');
+        tela.classList.remove('aceso');
+        tela.innerHTML = ''; 
+        IsLigado = false;
+        } 
+ });
+
+function desligarPokedex() {
+    isLigado = false;
+    botaoOn.classList.remove('aceso');
+    tela.innerHTML = '';
+}
+
+botaoOn.addEventListener('click', ()=> {
+    if (!isLigado) {
+        isLigado = true;
+        botaoOn.classList.add('aceso');
+        tela.innerHTML = '<img src="caminho-da-sua-imagem/logo.gif" alt="Pokemon Logo" class="logo-animada">';
+    }
+});
+
+botaoOff.addEventListener('click', ()=> { 
+    desligarPokedex();
 });
